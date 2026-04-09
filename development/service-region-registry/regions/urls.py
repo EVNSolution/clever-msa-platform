fe4f349416d9136f9ff74fad16a51ee4b953ed91
OziinG <dlajiin@gmail.com>
2026-04-09 17:51:17 +0900

    chore: complete batch 1 linked child repo conversion

--- development/service-region-registry/regions/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from regions.views import HealthView, RegionDetailView, RegionListCreateView
-
-urlpatterns = [
-    path("", RegionListCreateView.as_view()),
-    path("<uuid:region_id>/", RegionDetailView.as_view()),
-    path("health/", HealthView.as_view()),
-]
